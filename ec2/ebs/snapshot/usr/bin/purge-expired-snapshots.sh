bf35cba6883b7d35d1410ad1dc1b9d9c986f5fae
Aurélien Requiem <aurelien@requiem.fr>
2014-08-14 13:53:20 +1000

    added script to purge expired snapshots

---
+++ ec2/ebs/snapshot/usr/bin/purge-expired-snapshots.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TODAY="$(date +%s)"
+
+AWS='/usr/local/bin/aws'
+JQ='/usr/bin/jq'
+
+if [ ! -x "$JQ" ]
+then
+	echo "$JQ is not installed. (apt-get|yum) install jq"
+	exit 1
+fi
+
+if [ ! -x "$AWS" ]
+then
+	echo "AWS cli is not installed. visit: http://aws.amazon.com/cli/"
+	exit 1
+fi
+
+LOCAL_REGION=$(wget http://169.254.169.254/latest/meta-data/placement/availability-zone -q -O - | sed 's/.$//')
+
+for REGION in $($AWS --output json --region $LOCAL_REGION ec2 describe-regions | jq '.Regions[].RegionName' | sed 's/"//g')
+do
+	for SNAPSHOTID in $($AWS --output json --region $REGION ec2 describe-snapshots --filters "Name=tag-key,Values=Expire" | jq '.Snapshots[].SnapshotId' | sed 's/"//g')
+	do
+		EXPIRE=$($AWS --output json --region $REGION ec2 describe-snapshots --filters "Name=snapshot-id,Values=$SNAPSHOTID" | jq '.Snapshots[].Tags[]' | grep -A1 -B2 'Expire' | jq '.Value' | sed 's/"//g')
+		EXPIRE=$(date --date="$EXPIRE" +%s)
+
+		if [ "$EXPIRE" -le "$TODAY" ]
+		then
+			RETURN=$($AWS --output json --region $REGION ec2 delete-snapshot --snapshot-id "$SNAPSHOTID" | jq '.return' | sed 's/"//g')
+			if [ "$RETURN" != "true" ]
+			then
+				echo "Cannot delete snapshot $SNAPSHOTID"
+			fi
+		fi
+	done
+done
