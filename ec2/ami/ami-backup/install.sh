325d98df7454a5d61dcf2500663d5924ba360def
Aurélien Requiem <aurelien@requiem.fr>
2014-09-24 23:15:41 +1000

    added installation script to automate the deployment

---
+++ ec2/ami/ami-backup/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR=$(echo $0 | sed 's,[^/]*$,,; s,/$,,;')
+[ -z "$DIR" ] && DIR="."
+
+for x in usr/bin/purge-expired-amis.sh usr/bin/image-instance.sh
+do
+	cp ${DIR}/${x} /usr/bin/
+	chown root:root /${x}
+	chmod 0755 /${x}
+done
+
+if [ ! -f "/etc/cron.d/amicron" ]
+then
+	cp ${DIR}/etc/cron.d/amicron /etc/cron.d/
+	chown root:root /etc/cron.d/amicropn
+	chmod 0600 /etc/cron.d/amicron
+fi
+
+echo "Installation has finished, you need to edit /etc/cron.d/amicron"
+echo "and set your desired scheduling policy"
