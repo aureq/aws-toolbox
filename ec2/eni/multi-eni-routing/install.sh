9b6e84e545d00f61fc99d711aa31091ab25da499
Aurélien Requiem <aurelien@requiem.fr>
2014-10-25 10:48:11 +1100

    added installation support for Amazon Linux, Centos and Redhat

--- ec2/eni/multi-eni-routing/install.sh
+++ ec2/eni/multi-eni-routing/install.sh
@@ -10,11 +10,22 @@ do
 	chmod 0755 /${x}
 done
 
-DIST=$(lsb_release -i -s)
+if [ -x /usr/bin/lsb_release ]; then
+	DIST=$(lsb_release -i -s)
+else
+	[ -f /etc/centos-release ] && DIST=Centos
+	[ -f /etc/redhat-release ] && DIST=Redhat
+	[ -f /etc/system-release ] && DIST=Amazon
+fi
+
 case $DIST in
-	Debian | Ubuntu)
+	Debian|Ubuntu)
 		cp ${DIR}/etc/init.d/multi-eni-routing.Debian /etc/init.d/multi-eni-routing
 		update-rc.d multi-eni-routing defaults
+	;;
+	Amazon|Centos|Redhat)
+		cp ${DIR}/etc/init.d/multi-eni-routing.Redhat /etc/init.d/multi-eni-routing
+		chkconfig --add multi-eni-routing
 	;;
 esac
 
